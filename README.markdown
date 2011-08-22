49e1a2d683211af200f954b4bf65b3d823754de7
Jan Kus <jan@railslove.com>
2011-08-22 21:38:11 +0200

    first commit

---
+++ README.markdown
@@ -0,0 +1,19 @@
+[![Build Status](http://travis-ci.org/mkorfmann/NerdPursuit.png)](http://travis-ci.org/mkorfmann/NerdPursuit)
+
+Everybody loves recipes, but none of those let nerdism shine.
+
+## Participate
+
+Simply fork this repo, add your recipes and send us a pull request. 
+Use the comment feature for commits and pull requests or create a ticket.
+
+Real cookers will be added as team members.
+
+## Tech
+
+Recipes are submitted in JSON, with one question per file and must 
+align with our JSON-Schema.
+
+## How to Cook?
+
+It is up to you on how to use our recipes.
---
+++ validate_all.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'json-schema'
+require 'json'
+
+SCHEMA_FILE = File.join(File.dirname(File.expand_path(__FILE__)) , 'schema', 'recipes.json')
+RECIPES_PATH = File.join(File.dirname(File.expand_path(__FILE__)) , 'recipes')
+schema = File.open(SCHEMA_FILE) { |f| JSON.parse(f.read) }
+
+describe 'recipe' do
+  Dir["#{RECIPES_PATH}/**/*\.*"].each do |file|
+    it "#{file.sub(/(\..*$)/,'').sub(/^#{RECIPES_PATH}/,'')} contains valid json" do
+
+
+      begin
+        question = File.open(file) { |f| JSON.parse(f.read) }['recipe']
+        JSON::Validator.validate!(schema, question, version: :draft3 )
+      rescue JSON::Schema::ValidationError => schema_error
+         assertion =  false, schema_error.message
+      rescue JSON::ParserError => parser_error
+         assertion =  false, parser_error.message
+      else
+         assertion = true
+      end
+
+      assert *assertion
+    end
+  end
+end
